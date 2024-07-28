ce2f1593c251646e6498258ee7c02f374a8f19c8
anawsai <anapetrela8@gmail.com>
2024-07-27 20:17:11 -0400

    addition of encode function

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-def encode(password):
-    encoded_password = ''.join(str((int(char) + 3) % 10) for char in password)
-    return encoded_password
+def main():
+    encoded_password = None
+
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        option = input("Please enter an option: ")
+
+        if option == '1':
+            password = input("Please enter the password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+        elif option == '2':
+            if encoded_password:
+                original_password = decode(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
+            else:
+                print("No password has been encoded yet.")
+        elif option == '3':
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+if __name__ == "__main__":
+    main()
+
